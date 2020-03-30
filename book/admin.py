d63cb27af947a7b5a2dbf04ebac943c5162b3f66
adj6sd <2183014106@qq.com>
2020-03-30 17:09:40 +0800

    增加了book/admin的修饰类 初步完成了book/views中的函数

--- book/admin.py
+++ book/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Book
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['book_id', 'book_name', 'book_picture', 'price', 'price_old', 'author',
+                    'isbn', 'press', 'rest', 'kind_id', 'kind_name', 'description', 'sales']
+    list_filter = ['book_name']
+    search_fields = ['book_name']
+    list_per_page = 10
+    fieldsets = [
+        ("base", {"fields": ['book_id', 'book_name', 'press', 'author']}),
+        ("describe", {"fields": ['book_picture', 'isbn', 'kind_id', 'kind_name', 'description']}),
+        ("sale", {"fields": ['price', 'price_old', 'rest', 'sales']})
+    ]
+admin.site.register(Book, BookAdmin)
